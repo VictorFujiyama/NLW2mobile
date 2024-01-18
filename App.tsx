65f5b895576992f3022be84fbae25f27faca2a45
Victor <victorfujiyama@gmail.com>
2024-01-18 00:58:05 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Hello NLW</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +13,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize:32,
+    color: '#FFF',
+    fontWeight:'bold',
+  },
+
 });
