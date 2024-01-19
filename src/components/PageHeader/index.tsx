2a0f2a6240ff4612addb4253f0e56c95f6304cb0
Victor <victorfujiyama@gmail.com>
2024-01-19 18:13:07 -0300

    adding favorites

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import { View, Image, Text } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 
@@ -10,10 +10,12 @@ import { useNavigation } from "@react-navigation/native";
 
 interface PageHeaderProps {
     title: string;
+    children?: React.ReactNode; // Adicionando a propriedade children
+    headerRight?: ReactNode; 
 
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, children, headerRight }) => {
     const {navigate} = useNavigation(); 
 
     function handleGoBack() {
@@ -29,8 +31,14 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
                 
                 <Image source={logoImg} resizeMode="contain" />
             </View>
+            <View style={styles.header}>
 
-            <Text style={styles.title}>{title}</Text>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+
+            </View>
+
+            {children}
 
         </View>
     )
