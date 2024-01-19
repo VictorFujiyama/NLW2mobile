fa62cfe7d212e9c05f32f0c5c1869c7b48181c89
Victor <victorfujiyama@gmail.com>
2024-01-18 21:58:29 -0300

    adding giveClassesPage and StudyPages

--- App.tsx
+++ App.tsx
@@ -1,27 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import AppStack from './src/routes/AppStack';
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from  '@expo-google-fonts/archivo'
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular, 
+    Archivo_700Bold, 
+    Poppins_400Regular,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello NLW</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <AppStack />
+      <StatusBar style="light" />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  text: {
-    fontSize:32,
-    color: '#FFF',
-    fontWeight:'bold',
-  },
-
-});
